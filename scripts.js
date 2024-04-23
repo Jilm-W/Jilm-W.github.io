d4f81a611717d4f6e748d64cd54a0231ab570450
Jilm-W <158441687+Jilm-W@users.noreply.github.com>
2024-04-23 12:12:32 +0800

    Add files via upload

--- scripts.js
+++ scripts.js
@@ -1,25 +1,8 @@
-window.addEventListener('DOMContentLoaded', (event) => {
-    // 假设所有图片都包含 'lazy' 类
-    const images = document.querySelectorAll('.lazy');
-  
-    if ('IntersectionObserver' in window) {
-      const observer = new IntersectionObserver((entries) => {
-        entries.forEach(entry => {
-          if (entry.isIntersecting) {
-            const img = entry.target;
-            img.src = img.dataset.src;
-            img.classList.remove('lazy');
-            observer.unobserve(img);
-          }
-        });
-      }, { rootMargin: '30px' });
-  
-      images.forEach(img => observer.observe(img));
-    } else {
-      // 较老的浏览器不支持 Intersection Observer
-      images.forEach(img => {
-        img.src = img.dataset.src;
-        img.classList.remove('lazy');
-      });
-    }
+document.addEventListener('DOMContentLoaded', (event) => {
+  const hamburger = document.querySelector('.navbar-toggle');
+  const navMenu = document.querySelector('.navbar-menu');
+
+  hamburger.addEventListener('click', () => {
+    navMenu.classList.toggle('is-active');
   });
+});
